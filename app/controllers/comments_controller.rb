bf1746237a73ab71d28701c536ee67154f2beb9f
iamrubyist <takagioffcial4@gmail.com>
2021-02-24 18:17:10 +0900

    空でコメントできないようにした
    @commentsがコントローラーで定義できていなかったので、sを付け足して再定義しました

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params)
+    # binding.pry
     if @comment.save
       redirect_to  prototype_path(@comment.prototype)
     else
       @prototype = @comment.prototype
-      @comment = @prototype.comments
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
